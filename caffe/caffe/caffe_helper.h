8d3d2e2c925cba82a6fdc571e56053433a99e14e
Jiezju <65488866+Jiezju@users.noreply.github.com>
2021-08-11 18:32:24 +0800

    update helper function and slice test

--- caffe/caffe/caffe_helper.h
+++ caffe/caffe/caffe_helper.h
@@ -10,6 +10,44 @@
 using namespace caffe;
 
 namespace CaffeHelper{
+	void caffe_forward(boost::shared_ptr< Net<float> > & net, float *data_ptr)
+	{
+		Blob<float>* input_blobs = net->input_blobs()[0];
+		memcpy(input_blobs->mutable_cpu_data(), data_ptr,
+			sizeof(float)* input_blobs->count());
+		net->Forward();
+	}
+
+	//! Note: Layer包括神经网络所有层，比如，CaffeNet共有23层
+	// char *query_layer_name = "conv1";
+	unsigned int get_layer_index(boost::shared_ptr< Net<float> > & net, string query_layer_name)
+	{
+		std::string str_query(query_layer_name);
+		vector< string > const & layer_names = net->layer_names(); // 获取所有的layer name
+		for (unsigned int i = 0; i != layer_names.size(); ++i)
+		{
+			if (str_query == layer_names[i])
+			{
+				return i;
+			}
+		}
+		LOG(FATAL) << "Unknown layer name: " << str_query;
+	}
+
+	unsigned int get_blob_index(boost::shared_ptr< Net<float> > & net, string query_blob_name)
+	{
+		std::string str_query(query_blob_name);
+		vector< string > const & blob_names = net->blob_names();
+		for (unsigned int i = 0; i != blob_names.size(); ++i)
+		{
+			if (str_query == blob_names[i])
+			{
+				return i;
+			}
+		}
+		LOG(FATAL) << "Unknown blob name: " << str_query;
+	}
+
 	float* _generate_random_input(int num_size)
 	{
 		srand(time(NULL));
---
+++ caffe/caffe/slice_test.cpp
@@ -0,0 +1,41 @@
+#include "caffe_helper.h"
+
+
+int main()
+{
+	string net_file = "slice.prototxt";      //prototxtÎÄ¼þ
+	Caffe::set_mode(Caffe::CPU);
+	//Caffe::SetDevice(0);
+	Phase phase = TEST;
+	boost::shared_ptr<Net<float>> net(new caffe::Net<float>(net_file, phase));
+
+	string scale_layer_name = "layer2-scale";
+
+	int layer_idx = CaffeHelper::get_layer_index(net, scale_layer_name);
+
+	boost::shared_ptr<Layer<float>> scale_layer = net->layers()[layer_idx];
+	vector<boost::shared_ptr<Blob<float>>> scale_blobs = scale_layer->blobs();
+
+	// init input
+	float* input_ptr = CaffeHelper::_generate_random_input(net->input_blobs()[0]->count());
+
+	// init weight
+	float* weight_ptr = scale_blobs[0]->mutable_cpu_data();
+	float* bias_ptr = scale_blobs[1]->mutable_cpu_data();
+
+	float* muta_weight = CaffeHelper::_generate_random_input(scale_blobs[0]->count());
+	float* muta_bias = CaffeHelper::_generate_random_input(scale_blobs[1]->count());
+
+	memcpy(weight_ptr, muta_weight, scale_blobs[0]->count()*sizeof(float));
+	memcpy(bias_ptr, muta_bias, scale_blobs[1]->count()*sizeof(float));
+
+	CaffeHelper::caffe_forward(net, input_ptr);
+
+	vector<Blob<float>*> output_blobs = net->output_blobs();
+
+	
+	delete[] muta_weight;
+	delete[] muta_bias;
+
+	return 0;
+}
