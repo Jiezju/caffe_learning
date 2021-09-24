413fc11bedaefda149b26789694b88c7e532cf65
Bright <65488866+Jiezju@users.noreply.github.com>
2021-09-24 05:32:23 -0500

    update roi pooling layer reading

--- caffe/caffe/src/caffe/layers/roi_pooling_layer.cpp
+++ caffe/caffe/src/caffe/layers/roi_pooling_layer.cpp
@@ -21,7 +21,7 @@ void ROIPoolingLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
       << "pooled_w must be > 0";
   pooled_height_ = roi_pool_param.pooled_h();
   pooled_width_ = roi_pool_param.pooled_w();
-  spatial_scale_ = roi_pool_param.spatial_scale();
+  spatial_scale_ = roi_pool_param.spatial_scale(); // 这个是该层特征图相对原图的下采样率，因为roi的坐标值都是基于原图的
   LOG(INFO) << "Spatial scale: " << spatial_scale_;
 }
 
@@ -34,7 +34,7 @@ void ROIPoolingLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
   top[0]->Reshape(bottom[1]->num(), channels_, pooled_height_,
       pooled_width_);
   max_idx_.Reshape(bottom[1]->num(), channels_, pooled_height_,
-      pooled_width_);
+      pooled_width_); // 存储 pooling 值对应的 index，用于反向传播
 }
 
 template <typename Dtype>
@@ -47,33 +47,39 @@ void ROIPoolingLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
   int batch_size = bottom[0]->num();
   int top_count = top[0]->count();
   Dtype* top_data = top[0]->mutable_cpu_data();
-  caffe_set(top_count, Dtype(-FLT_MAX), top_data);
+  caffe_set(top_count, Dtype(-FLT_MAX), top_data); // 初始化top_data 为最小负数
   int* argmax_data = max_idx_.mutable_cpu_data();
-  caffe_set(top_count, -1, argmax_data);
+  caffe_set(top_count, -1, argmax_data); // 初始化 argmax_data 为-1
 
   // For each ROI R = [batch_index x1 y1 x2 y2]: max pool over R
   for (int n = 0; n < num_rois; ++n) {
-    int roi_batch_ind = bottom_rois[0];
-    int roi_start_w = round(bottom_rois[1] * spatial_scale_);
-    int roi_start_h = round(bottom_rois[2] * spatial_scale_);
-    int roi_end_w = round(bottom_rois[3] * spatial_scale_);
-    int roi_end_h = round(bottom_rois[4] * spatial_scale_);
+	/* 取出单个 roi的5个值 [batch_index x1 y1 x2 y2] index + 坐标 */ 
+    int roi_batch_ind = bottom_rois[0]; // batch_index
+    int roi_start_w = round(bottom_rois[1] * spatial_scale_); // x1
+    int roi_start_h = round(bottom_rois[2] * spatial_scale_); // y1
+    int roi_end_w = round(bottom_rois[3] * spatial_scale_); // x2
+    int roi_end_h = round(bottom_rois[4] * spatial_scale_); // y2
+	
+	// 仅支持roi的batch为1（即图像的batch）
     CHECK_GE(roi_batch_ind, 0);
     CHECK_LT(roi_batch_ind, batch_size);
 
+	// 计算 roi 宽高尺寸
     int roi_height = max(roi_end_h - roi_start_h + 1, 1);
     int roi_width = max(roi_end_w - roi_start_w + 1, 1);
+
+	// 比例 roi 尺寸/ pooling尺寸  可以看成每个 pooling 的一个单元相对于原图的大小
     const Dtype bin_size_h = static_cast<Dtype>(roi_height)
                              / static_cast<Dtype>(pooled_height_);
     const Dtype bin_size_w = static_cast<Dtype>(roi_width)
                              / static_cast<Dtype>(pooled_width_);
-
+	// 获取batch bottom指针
     const Dtype* batch_data = bottom_data + bottom[0]->offset(roi_batch_ind);
 
     for (int c = 0; c < channels_; ++c) {
       for (int ph = 0; ph < pooled_height_; ++ph) {
         for (int pw = 0; pw < pooled_width_; ++pw) {
-          // Compute pooling region for this output unit:
+          // Compute pooling region for this output unit: 将此时输出的坐标映射到pooling的特征图区域
           //  start (included) = floor(ph * roi_height / pooled_height_)
           //  end (excluded) = ceil((ph + 1) * roi_height / pooled_height_)
           int hstart = static_cast<int>(floor(static_cast<Dtype>(ph)
@@ -85,6 +91,7 @@ void ROIPoolingLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
           int wend = static_cast<int>(ceil(static_cast<Dtype>(pw + 1)
                                            * bin_size_w));
 
+          // 对获取 pooling 区域的clip以及合法性验证， 并将转换成全图坐标 [wstart, hstart, wend, hend] -> [wstart + roi_start_w, hstart + roi_start_h, wend + roi_start_w, hend + roi_start_h]
           hstart = min(max(hstart + roi_start_h, 0), height_);
           hend = min(max(hend + roi_start_h, 0), height_);
           wstart = min(max(wstart + roi_start_w, 0), width_);
@@ -98,6 +105,7 @@ void ROIPoolingLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
             argmax_data[pool_index] = -1;
           }
 
+          // 区域max pooling的过程
           for (int h = hstart; h < hend; ++h) {
             for (int w = wstart; w < wend; ++w) {
               const int index = h * width_ + w;
@@ -109,13 +117,13 @@ void ROIPoolingLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
           }
         }
       }
-      // Increment all data pointers by one channel
+      // Increment all data pointers by one channel 每个数据指针向后移动一个channel
       batch_data += bottom[0]->offset(0, 1);
       top_data += top[0]->offset(0, 1);
       argmax_data += max_idx_.offset(0, 1);
     }
     // Increment ROI data pointer
-    bottom_rois += bottom[1]->offset(1);
+    bottom_rois += bottom[1]->offset(1); // <==> bottom_rois += 5
   }
 }
 
